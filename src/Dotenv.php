199813ea19602eab8fc95dddf242b4859aa97e40
Amir Ali Salah <codeurx@gmail.com>
2018-04-24 17:29:19 +0200

    Self Load

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -14,6 +14,7 @@ class Dotenv
     {
         $this->filePath = $this->getFilePath($path, $file);
         $this->loader = new Loader($this->filePath, true);
+        $this->load();
     }
 
     public function load()
